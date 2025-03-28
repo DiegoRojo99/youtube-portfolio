7845398b97b513560bdf58bdb134c6caf33dd1c7
DiegoRojo99 <diego99rojo@gmail.com>
2025-03-28 23:51:20 +0100

    Social links section with youtube created

--- src/app/components/SocialLinks.tsx
+++ src/app/components/SocialLinks.tsx
@@ -1,54 +1,40 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import SocialMediaCard from './SocialMediaCard';
 
-interface SocialLink {
-  id: number;
-  name: string;
-  logo: string;
-  followers: number;
-  videos: number;
-}
-
-const socialLinks: SocialLink[] = [
-  {
-    id: 1,
-    name: 'YouTube',
-    logo: '/assets/youtube-icon.png',
-    followers: 12000,
-    videos: 150,
-  },
-  {
-    id: 2,
-    name: 'Twitter',
-    logo: '/assets/twitter-icon.png',
-    followers: 8000,
-    videos: 0,
-  },
-  {
-    id: 3,
-    name: 'Instagram',
-    logo: '/assets/instagram-icon.png',
-    followers: 15000,
-    videos: 0,
-  },
-];
-
-const SocialLinkCard: React.FC<SocialLink> = ({ name, logo, followers, videos }) => {
-  return (
-    <div className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center text-center 
-      transition-transform transform hover:scale-105 hover:shadow-lg cursor-pointer">
-      <img src={logo} alt={`${name} logo`} className="w-16 h-16 mb-4" />
-      <h3 className="text-lg font-semibold">{name}</h3>
-      <p className="text-gray-600">Followers: {followers}</p>
-      {videos > 0 && <p className="text-gray-600">Videos: {videos}</p>}
-    </div>
-  );
+type YoutubeStats = {
+  subscriberCount: number;
+  videoCount: number;
+  viewCount: number;
 };
 
 const SocialLinks: React.FC = () => {
+  const [youtubeStats, setYoutubeStats] = useState<YoutubeStats | null>(null);
+
+  useEffect(() => {
+    const fetchSocialMediaStats = async () => {
+      const res = await fetch('/api/youtube');
+      const data = await res.json();
+      setYoutubeStats(data);
+    };
+
+    fetchSocialMediaStats();
+  }, []);
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
-      {socialLinks.map((link) => <SocialLinkCard key={link.id} {...link} />)}
-    </div>
+    <>
+      <h1 className="text-3xl font-bold text-center mt-8 mb-4">Redes Sociales</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4 justify-around">
+        <SocialMediaCard 
+          logo="/assets/youtube-icon.png"
+          followers={youtubeStats?.subscriberCount ?? 0}
+          videos={youtubeStats?.videoCount ?? 0}
+          views={youtubeStats?.viewCount ?? 0}
+          name="YouTube"
+          onFollow={() => window.open('https://www.youtube.com/@santimercadal', '_blank')}
+        />
+      </div>
+    </>
   );
 };
 
