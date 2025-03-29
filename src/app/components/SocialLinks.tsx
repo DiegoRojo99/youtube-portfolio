d784f91ed6d7db2224f84ee94275f84e10006e80
DiegoRojo99 <diego99rojo@gmail.com>
2025-03-29 14:42:26 +0100

    Added TikTok card

--- src/app/components/SocialLinks.tsx
+++ src/app/components/SocialLinks.tsx
@@ -1,19 +1,15 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import YoutubeCard from './SocialMedia/YoutubeCard';
-
-type YoutubeStats = {
-  subscriberCount: number;
-  videoCount: number;
-  viewCount: number;
-};
+import TikTokCard from './SocialMedia/TikTokCard';
 
 const SocialLinks: React.FC = () => {
   return (
     <div className="bg-white text-black flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-center mt-8 mb-4">Redes Sociales</h1>
-      <div className="flex flex-wrap justify-center gap-4 mb-8">
+      {/* <h1 className="text-3xl font-bold text-center mt-8 mb-4">Redes Sociales</h1> */}
+      <div className="flex flex-wrap justify-center mt-4 mb-8">
         <YoutubeCard />
+        <TikTokCard />
       </div>
     </div>
   );
