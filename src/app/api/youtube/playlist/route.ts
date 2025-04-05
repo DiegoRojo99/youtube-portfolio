56912453d1a44375357dc949352766cd5ca34565
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-05 12:42:07 +0200

    Created endpoint to pull playlist data from YT

---
+++ src/app/api/youtube/playlist/route.ts
@@ -0,0 +1,31 @@
+import { YoutubeSearchDataItem } from '@/app/types/YouTube';
+import { NextResponse } from 'next/server';
+
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+const BASE_URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const playlistId = searchParams.get('playlistId');
+
+  if (!playlistId) {
+    return NextResponse.json({ error: 'Missing playlistId parameter' }, { status: 400 });
+  }
+
+  try {
+    const response = await fetch(
+      `${BASE_URL}?part=snippet&playlistId=${playlistId}&key=${YOUTUBE_API_KEY}&maxResults=50`
+    );
+
+    if (!response.ok) {
+      return NextResponse.json({ error: 'Failed to fetch playlist from YouTube' }, { status: response.status });
+    }
+
+    const data = await response.json();
+    const videos = data.items;
+    return NextResponse.json(videos, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching playlist:', error);
+    return NextResponse.json({ error: 'An error occurred while fetching the playlist' }, { status: 500 });
+  }
+}
