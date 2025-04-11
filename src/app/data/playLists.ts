78313484caf69fe833c793724b60c3d95d0a6b44
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-11 20:29:20 +0200

    Reduced playlists info

--- src/app/data/playLists.ts
+++ src/app/data/playLists.ts
@@ -1,217 +1,37 @@
-let playLists = [
+const playLists = [
   {
-      "kind": "youtube#playlist",
-      "etag": "iDuD-3n7piJFmUGIqpAwYkL1FDA",
       "id": "PLBCFzf3IR5m4bw-DPgMX-Z6J9eujeBTNb",
       "snippet": {
-          "publishedAt": "2024-09-11T11:01:52.587128Z",
-          "channelId": "UCSRNA6scha456AojOjmkh3w",
           "title": "Vídeos Comida",
-          "description": "",
-          "thumbnails": {
-              "default": {
-                  "url": "https://i.ytimg.com/vi/P0WpRVT_y4M/default.jpg",
-                  "width": 120,
-                  "height": 90
-              },
-              "medium": {
-                  "url": "https://i.ytimg.com/vi/P0WpRVT_y4M/mqdefault.jpg",
-                  "width": 320,
-                  "height": 180
-              },
-              "high": {
-                  "url": "https://i.ytimg.com/vi/P0WpRVT_y4M/hqdefault.jpg",
-                  "width": 480,
-                  "height": 360
-              },
-              "standard": {
-                  "url": "https://i.ytimg.com/vi/P0WpRVT_y4M/sddefault.jpg",
-                  "width": 640,
-                  "height": 480
-              },
-              "maxres": {
-                  "url": "https://i.ytimg.com/vi/P0WpRVT_y4M/maxresdefault.jpg",
-                  "width": 1280,
-                  "height": 720
-              }
-          },
-          "channelTitle": "Santi Mercadal",
-          "localized": {
-              "title": "Vídeos Comida",
-              "description": ""
-          }
+          "description": ""
       }
   },
   {
-      "kind": "youtube#playlist",
-      "etag": "DbYc_TolqOMnTjE21Mz4e-rBONg",
       "id": "PLBCFzf3IR5m6C7H1LZsk1w3rbvqzkSWAv",
       "snippet": {
-          "publishedAt": "2024-09-11T11:01:40.030567Z",
-          "channelId": "UCSRNA6scha456AojOjmkh3w",
           "title": "Viajes",
           "description": "",
-          "thumbnails": {
-              "default": {
-                  "url": "https://i.ytimg.com/vi/oJfOTOsdO-0/default.jpg",
-                  "width": 120,
-                  "height": 90
-              },
-              "medium": {
-                  "url": "https://i.ytimg.com/vi/oJfOTOsdO-0/mqdefault.jpg",
-                  "width": 320,
-                  "height": 180
-              },
-              "high": {
-                  "url": "https://i.ytimg.com/vi/oJfOTOsdO-0/hqdefault.jpg",
-                  "width": 480,
-                  "height": 360
-              },
-              "standard": {
-                  "url": "https://i.ytimg.com/vi/oJfOTOsdO-0/sddefault.jpg",
-                  "width": 640,
-                  "height": 480
-              },
-              "maxres": {
-                  "url": "https://i.ytimg.com/vi/oJfOTOsdO-0/maxresdefault.jpg",
-                  "width": 1280,
-                  "height": 720
-              }
-          },
-          "channelTitle": "Santi Mercadal",
-          "localized": {
-              "title": "Viajes",
-              "description": ""
-          }
       }
   },
   {
-      "kind": "youtube#playlist",
-      "etag": "mFwqv-5dBfKYnrYKDhvzmRsh_40",
       "id": "PLBCFzf3IR5m6q9vcbLaRA2baVm868KVg6",
       "snippet": {
-          "publishedAt": "2024-08-25T10:22:13.220935Z",
-          "channelId": "UCSRNA6scha456AojOjmkh3w",
           "title": "Deportes Rurales Vascos (Herri Kirolak",
           "description": "",
-          "thumbnails": {
-              "default": {
-                  "url": "https://i.ytimg.com/vi/ZhkcCE4R3hA/default.jpg",
-                  "width": 120,
-                  "height": 90
-              },
-              "medium": {
-                  "url": "https://i.ytimg.com/vi/ZhkcCE4R3hA/mqdefault.jpg",
-                  "width": 320,
-                  "height": 180
-              },
-              "high": {
-                  "url": "https://i.ytimg.com/vi/ZhkcCE4R3hA/hqdefault.jpg",
-                  "width": 480,
-                  "height": 360
-              },
-              "standard": {
-                  "url": "https://i.ytimg.com/vi/ZhkcCE4R3hA/sddefault.jpg",
-                  "width": 640,
-                  "height": 480
-              },
-              "maxres": {
-                  "url": "https://i.ytimg.com/vi/ZhkcCE4R3hA/maxresdefault.jpg",
-                  "width": 1280,
-                  "height": 720
-              }
-          },
-          "channelTitle": "Santi Mercadal",
-          "localized": {
-              "title": "Deportes Rurales Vascos (Herri Kirolak",
-              "description": ""
-          }
       }
   },
   {
-      "kind": "youtube#playlist",
-      "etag": "iRCD-A9xPCAPURDsgOyKbNHvCC8",
       "id": "PLBCFzf3IR5m45Mq2gQXJLbjFAHvxWsGpX",
       "snippet": {
-          "publishedAt": "2024-08-25T10:21:52.474384Z",
-          "channelId": "UCSRNA6scha456AojOjmkh3w",
           "title": "Deportes Rurales",
           "description": "",
-          "thumbnails": {
-              "default": {
-                  "url": "https://i.ytimg.com/vi/ZhkcCE4R3hA/default.jpg",
-                  "width": 120,
-                  "height": 90
-              },
-              "medium": {
-                  "url": "https://i.ytimg.com/vi/ZhkcCE4R3hA/mqdefault.jpg",
-                  "width": 320,
-                  "height": 180
-              },
-              "high": {
-                  "url": "https://i.ytimg.com/vi/ZhkcCE4R3hA/hqdefault.jpg",
-                  "width": 480,
-                  "height": 360
-              },
-              "standard": {
-                  "url": "https://i.ytimg.com/vi/ZhkcCE4R3hA/sddefault.jpg",
-                  "width": 640,
-                  "height": 480
-              },
-              "maxres": {
-                  "url": "https://i.ytimg.com/vi/ZhkcCE4R3hA/maxresdefault.jpg",
-                  "width": 1280,
-                  "height": 720
-              }
-          },
-          "channelTitle": "Santi Mercadal",
-          "localized": {
-              "title": "Deportes Rurales",
-              "description": ""
-          }
       }
   },
   {
-      "kind": "youtube#playlist",
-      "etag": "c0JMo6DOulB-DvMQgHXt2OrqUVE",
       "id": "PLBCFzf3IR5m7y9nVXGOTKRdIbnhl_U-zf",
       "snippet": {
-          "publishedAt": "2024-07-27T16:48:36.510884Z",
-          "channelId": "UCSRNA6scha456AojOjmkh3w",
           "title": "España Oculta",
           "description": "En esta playlist encontrás vlogs y documentales sobre las tradicionales más especiales de España",
-          "thumbnails": {
-              "default": {
-                  "url": "https://i.ytimg.com/vi/iZrCElDmixY/default.jpg",
-                  "width": 120,
-                  "height": 90
-              },
-              "medium": {
-                  "url": "https://i.ytimg.com/vi/iZrCElDmixY/mqdefault.jpg",
-                  "width": 320,
-                  "height": 180
-              },
-              "high": {
-                  "url": "https://i.ytimg.com/vi/iZrCElDmixY/hqdefault.jpg",
-                  "width": 480,
-                  "height": 360
-              },
-              "standard": {
-                  "url": "https://i.ytimg.com/vi/iZrCElDmixY/sddefault.jpg",
-                  "width": 640,
-                  "height": 480
-              },
-              "maxres": {
-                  "url": "https://i.ytimg.com/vi/iZrCElDmixY/maxresdefault.jpg",
-                  "width": 1280,
-                  "height": 720
-              }
-          },
-          "channelTitle": "Santi Mercadal",
-          "localized": {
-              "title": "España Oculta",
-              "description": "En esta playlist encontrás vlogs y documentales sobre las tradicionales más especiales de España"
-          }
       }
   }
 ]
