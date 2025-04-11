12caab6be9108521170db416b362f6eff6b2ba4d
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-11 17:41:03 +0200

    Playlist text color and padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <Hero />
       <LatestYouTubeVideo />
       <SocialLinks />
-      <YouTubePlayListSection playlistId="PLBCFzf3IR5m7y9nVXGOTKRdIbnhl_U-zf" title="España Oculta" imageUrl="" />
-      <YouTubePlayListSection playlistId="PLBCFzf3IR5m6C7H1LZsk1w3rbvqzkSWAv" title="Viajes" imageUrl="travelPostCards.jpg" />
+      <YouTubePlayListSection playlistId="PLBCFzf3IR5m7y9nVXGOTKRdIbnhl_U-zf" title="España Oculta" imageUrl="" textColor="white" />
+      <YouTubePlayListSection playlistId="PLBCFzf3IR5m6C7H1LZsk1w3rbvqzkSWAv" title="Viajes" imageUrl="travelPostCards.jpg" textColor="black" />
     </Layout>
   );
 }
