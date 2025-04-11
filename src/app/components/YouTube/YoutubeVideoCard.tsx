d0c694374754356f9938b16577bd54a0ed1efb08
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-11 20:16:06 +0200

    Updated card style

--- src/app/components/YouTube/YoutubeVideoCard.tsx
+++ src/app/components/YouTube/YoutubeVideoCard.tsx
@@ -6,33 +6,42 @@ interface Props {
 }
 
 export default function YoutubeVideoCard({ video }: Props) {
+  if (!video?.snippet) return <></>;
 
   return (
-    <div className="relative group w-64 flex-shrink-0 cursor-pointer transition-transform duration-300 hover:scale-105">
-      <a href={`https://www.youtube.com/watch?v=${video.id}`} target="_blank" rel="noopener noreferrer">
-      {
-        video.snippet.thumbnails.maxres ? (
+    <div className="w-72 flex-shrink-0 cursor-pointer transition-transform duration-300 group">
+      <a
+        href={`https://www.youtube.com/watch?v=${video.snippet?.resourceId?.videoId}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="block rounded-lg overflow-hidden transform transition-transform duration-300 group-hover:-translate-y-2"
+      >
+        <div className="relative">
           <Image
-            src={video.snippet.thumbnails.maxres.url}
+            src={
+              video.snippet.thumbnails.maxres
+                ? video.snippet.thumbnails.maxres.url
+                : video.snippet.thumbnails.high.url
+            }
             alt={video.snippet.title}
-            width={video.snippet.thumbnails.maxres.width}
-            height={video.snippet.thumbnails.maxres.height}
-            className='rounded-lg w-full object-cover'
+            width={
+              video.snippet.thumbnails.maxres
+                ? video.snippet.thumbnails.maxres.width
+                : video.snippet.thumbnails.high.width
+            }
+            height={
+              video.snippet.thumbnails.maxres
+                ? video.snippet.thumbnails.maxres.height
+                : video.snippet.thumbnails.high.height
+            }
+            className="rounded-lg w-full object-cover"
           />
-        ) : (
-          <Image
-            src={video.snippet.thumbnails.high.url}
-            alt={video.snippet.title}
-            width={video.snippet.thumbnails.high.width}
-            height={video.snippet.thumbnails.high.height}
-            className='rounded-lg w-full object-cover'
-          />
-        )
-      }
+        </div>
       </a>
-      <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 text-white text-sm p-2 opacity-0 group-hover:opacity-100 transition-opacity">
+
+      <p className="mt-2 text-white text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300">
         {video.snippet.title}
-      </div>
+      </p>
     </div>
   );
 }
