2fa386d2db782a498223d1a59d0638f776db6932
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-14 12:48:59 +0200

    Created error component

---
+++ src/app/components/Extras/Error.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Extras.css';
+
+const ErrorWarning: React.FC<{ title: string }> = ({ title }) => {
+  return (
+    <div className='d-flex flex-col justify-center items-center py-12'>
+      <img src={'/assets/warning.svg'} alt="Warning" className='white-image w-64 mx-auto' />
+      <h2 style={{ color: '#fff' }}>
+        {title}
+      </h2>
+    </div>
+  );
+};
+
+export default ErrorWarning;
