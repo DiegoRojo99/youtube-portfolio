1693cc99c29061f3ed7863c7fd0f1bb12fd75ab3
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-15 17:10:11 +0200

    Error warning h2 style

--- src/app/components/Extras/Error.tsx
+++ src/app/components/Extras/Error.tsx
@@ -5,7 +5,7 @@ const ErrorWarning: React.FC<{ title: string }> = ({ title }) => {
   return (
     <div className='d-flex flex-col justify-center items-center py-12'>
       <img src={'/assets/warning.svg'} alt="Warning" className='white-image w-64 mx-auto' />
-      <h2 style={{ color: '#fff' }}>
+      <h2 style={{ color: '#fff', textAlign: 'center', fontSize: '2rem' }}>
         {title}
       </h2>
     </div>
