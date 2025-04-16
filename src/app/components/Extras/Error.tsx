bf20cdba32d75df5705e2caa44afbf94563797d7
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-16 13:43:19 +0200

    Updated img to next image

--- src/app/components/Extras/Error.tsx
+++ src/app/components/Extras/Error.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Extras.css';
+import Image from 'next/image';
 
 const ErrorWarning: React.FC<{ title: string }> = ({ title }) => {
   return (
     <div className='d-flex flex-col justify-center items-center py-12'>
-      <img src={'/assets/warning.svg'} alt="Warning" className='white-image w-64 mx-auto' />
+      <Image src={'/assets/warning.svg'} alt="Warning" className='white-image w-64 mx-auto' />
       <h2 style={{ color: '#fff', textAlign: 'center', fontSize: '2rem' }}>
         {title}
       </h2>
